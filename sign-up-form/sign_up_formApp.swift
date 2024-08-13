bab6fa3f4207e8482e833b4a76e771677e67c595
Dimash <dimashaijarikov@gmail.com>
2024-08-13 16:28:40 +0500

    Initial Commit

---
+++ sign-up-form/sign_up_formApp.swift
@@ -0,0 +1,17 @@
+//
+//  sign_up_formApp.swift
+//  sign-up-form
+//
+//  Created by Динмуханбет Айжарыков on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct sign_up_formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
