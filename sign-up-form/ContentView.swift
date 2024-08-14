39506aa5a6adc4c8581ac1230712b68c5f7c607c
Dimash <dimashaijarikov@gmail.com>
2024-08-14 16:51:00 +0500

    update

--- sign-up-form/ContentView.swift
+++ sign-up-form/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { _ in
+            VStack {
+                Image(systemName: "lock.open.display").resizable().frame(width: 70, height: 70)
+                
+                ZStack {
+                    Text("Hello World")
+                    
+                }
+                HStack(spacing: 15) {
+                    Rectangle().fill(Color.gray).frame(height: 1)
+                    Image(systemName: "dot.scope.display")
+                    Rectangle().fill(Color.gray).frame(height: 1)
+                }.padding(.horizontal, 30).padding(.top, 30)
+                
+                HStack (spacing: 15) {
+                    Button(action: {
+                        //
+                    }) {
+                        Image(systemName: "globe.central.south.asia").renderingMode(.original).frame(width: 50, height: 50).clipShape(Circle())
+                    }
+                    Button(action: {
+                        //
+                    }) {
+                        Image(systemName: "sun.max.circle").renderingMode(.original).frame(width: 50, height: 50).clipShape(Circle())
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
